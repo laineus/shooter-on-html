1ea26e847752b29114005f44c7b3e739d5e19e84
laineus <lainen000@gmail.com>
2019-04-14 18:26:08 +0900

    inject src in background

--- js/background.js
+++ js/background.js
@@ -1,3 +1,9 @@
-chrome.browserAction.onClicked.addListener(() => {
+const alreadyInjectedTabs = []
+chrome.browserAction.onClicked.addListener(tab => {
+  if (!alreadyInjectedTabs.includes(tab.id)) {
+    alreadyInjectedTabs.push(tab.id)
+    chrome.tabs.executeScript(null, { file: 'js/index.js' })
+    chrome.tabs.insertCSS(null, { file: 'css/style.css' })
+  }
   chrome.tabs.executeScript(null, { file: 'js/initialize.js' })
 })
