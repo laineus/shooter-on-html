4e7e2029559e381d1a2443699a05279e9f2a4085
laineus <lainen000@gmail.com>
2019-04-14 19:50:01 +0900

    fix injection

--- js/background.js
+++ js/background.js
@@ -1,9 +1,14 @@
-const alreadyInjectedTabs = []
-chrome.browserAction.onClicked.addListener(tab => {
-  if (!alreadyInjectedTabs.includes(tab.id)) {
-    alreadyInjectedTabs.push(tab.id)
+let alreadyInjected = false
+
+chrome.browserAction.onClicked.addListener(() => {
+  if (!alreadyInjected) {
+    alreadyInjected = true
     chrome.tabs.executeScript(null, { file: 'js/index.js' })
     chrome.tabs.insertCSS(null, { file: 'css/style.css' })
   }
   chrome.tabs.executeScript(null, { file: 'js/initialize.js' })
+})
+
+chrome.tabs.onUpdated.addListener(() => {
+  alreadyInjected = false
 })
